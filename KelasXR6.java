215abbae3106931604b4741cfeecc937b73360be
Sepvaldi <sepvalditunisia@gmail.com>
2025-08-05 08:53:06 +0700

    Add files via upload
    tugas biodata

---
+++ KelasXR6.java
@@ -0,0 +1,11 @@
+public class KelasXR6 {
+    public static void main(String[] args) {
+        System.out.println("Nama : Sepvaldi Firmandah Ramadhan");
+        System.out.println("Asal Sekolah : MTS Khadijah Malang" );
+        System.out.println("Alamat Rumah : Jl Gadang Gang 12 No'30 Sukun Malang");
+        System.out.println("Jenis Kelamin : Laki-Laki");
+        System.out.println("Cita-cita : Menjadi Ahli IT");
+        System.out.println("Hobi : Bermain Game, Berenang");
+        
+    }
+}
